f5add589db5d7d105a6468c1d0561c891c258d00
abiisaleh <abi.saleh.25@gmail.com>
2023-10-28 19:45:52 +0900

    update widget chart

--- app/Filament/Widgets/StokDarah.php
+++ app/Filament/Widgets/StokDarah.php
@@ -20,12 +20,52 @@ class StokDarah extends BaseWidget
     {
         $darah_masuk = DarahMasuk::all()->count();
         $darah_keluar = PenggunaDarah::all()->sum('jumlah_kolf');
-        $stok_darah = $darah_masuk - $darah_keluar;
+
+        function hitung($data)
+        {
+            foreach ($data as $key => $value) {
+                $chart[] = $value;
+            }
+
+            $result['chart'] = $chart;
+
+            $perubahan = 0;
+
+            foreach ($chart as $value) {
+                $perubahan = $value - $perubahan;
+            }
+
+            if ($perubahan < 0) {
+                $result['desc'] = ($perubahan * -1) . ' berkurang';
+                $result['icon'] = 'heroicon-m-arrow-trending-down';
+                $result['color'] = 'danger';
+            } elseif ($perubahan > 0) {
+                $result['desc'] = $perubahan . ' bertambah';
+                $result['icon'] = 'heroicon-m-arrow-trending-up';
+                $result['color'] = 'success';
+            } else {
+                $result['desc'] = 'stabil';
+                $result['icon'] = 'heroicon-m-arrow-long-right';
+                $result['color'] = 'warning';
+            }
+
+            return $result;
+        }
+
+        $data_darah_masuk = hitung(DarahMasuk::all()->countBy('tanggal')->toArray());
+        $data_darah_keluar = hitung(PenggunaDarah::all()->countBy('tanggal')->toArray());
 
         return [
-            // Stat::make('Stok Darah', $stok_darah),
-            Stat::make('Darah Masuk', $darah_masuk),
-            Stat::make('Darah Keluar', $darah_keluar),
+            Stat::make('Darah Masuk', $darah_masuk)
+                ->description($data_darah_masuk['desc'])
+                ->descriptionIcon($data_darah_masuk['icon'])
+                ->chart($data_darah_masuk['chart'])
+                ->color($data_darah_masuk['color']),
+            Stat::make('Darah Keluar', $darah_keluar)
+                ->description($data_darah_keluar['desc'])
+                ->descriptionIcon($data_darah_keluar['icon'])
+                ->chart($data_darah_keluar['chart'])
+                ->color($data_darah_keluar['color']),
         ];
     }
 }
