13f0ac8eecdd837edbd15b9470dc87d6877afe10
abiisaleh <abi.saleh.25@gmail.com>
2023-10-28 13:52:37 +0900

    add map widget

--- app/Filament/Widgets/StokDarah.php
+++ app/Filament/Widgets/StokDarah.php
@@ -4,6 +4,7 @@ namespace App\Filament\Widgets;
 
 use App\Models\DarahMasuk;
 use App\Models\PenggunaDarah;
+use Filament\Infolists\Components\Grid;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
